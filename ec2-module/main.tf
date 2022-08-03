cad817d090df13185f9b96cd527b06540e426629
Dallison Lima <dallisonlima@gmail.com>
2022-08-03 20:18:26 -0300

    att files

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,9 +1,4 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instancetype
-
-
-  tags = {
-    Name = var.environment == "teste" ? var.environment : "Valor era diferente de teste ai mudei"
-  }
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2-module" {
   source       = "./ec2-module"
-  ami          = "anbihsjehaiuse"
+  ami          = "ami-090fa75af13c156b4"
   instancetype = "t2.micro"
   environment = "dev"
 }
