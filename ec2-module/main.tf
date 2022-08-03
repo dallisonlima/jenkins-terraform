cdc62a48490b2fb56565c979ae88e2801774900e
Dallison Lima <dallisonlima@gmail.com>
2022-08-03 19:43:13 -0300

    add tf

---
+++ ec2-module/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instancetype
+
+
+  tags = {
+    Name = var.environment == "teste" ? var.environment : "Valor era diferente de teste ai mudei"
+  }
+}
