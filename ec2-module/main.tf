9b251f2cf913d8abd8acccdb036a6ed5b1ae7d9f
Dallison Lima <dallisonlima@gmail.com>
2022-08-24 23:46:36 -0300

    att module

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,4 +1,13 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instancetype
+  user_data = <<EOF
+    #!/bin/bash
+
+    sudo yum update -y
+
+    sudo yum install -y gcc
+
+    sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
+  EOF
 }
