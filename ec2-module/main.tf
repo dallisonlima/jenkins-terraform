b08d8f4007c5724ede28dcf8ef48d951a64e3c71
Dallison Lima <dallisonlima@gmail.com>
2022-08-25 00:52:11 -0300

    att module

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y git
-    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+    wait git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
     sudo mkdir ~/.linuxbrew/bin
     sudo ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
     sudo eval $(~/.linuxbrew/bin/brew shellenv)
