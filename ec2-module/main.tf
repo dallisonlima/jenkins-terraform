4c4d1ca60a973b8b917338144e52aec8ad67c57a
Dallison Lima <dallisonlima@gmail.com>
2022-08-24 23:52:36 -0300

    att module

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instancetype
+  key_name = "homebrew-teste"
   user_data = <<EOF
     #!/bin/bash
 
@@ -10,4 +11,8 @@ resource "aws_instance" "web" {
 
     sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
   EOF
+
+  tags = {
+    Name = "CriadaByJenkinsPipeline"
+  }
 }
